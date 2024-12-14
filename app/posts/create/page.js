74d7c01229f8ba9e7cc7883ee3a85215035a2852
unknown <bashawdejenu1221@gmail.com>
2024-12-14 12:04:36 +0300

    create create  page

---
+++ app/posts/create/page.js
@@ -0,0 +1,16 @@
+"use client"
+
+import PostForm from '../../../components/PostForm';
+import { createPost } from '../../../utils/api';
+import { useRouter } from 'next/navigation';
+
+export default function CreatePostPage() {
+  const router = useRouter();
+
+  const handleSubmit = async (data) => {
+    await createPost(data);
+    router.push('/posts');
+  };
+
+  return <PostForm onSubmit={handleSubmit} />;
+}
