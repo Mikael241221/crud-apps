1834a7cfd0fe1f86ff286524d82062d1b5228e7d
unknown <bashawdejenu1221@gmail.com>
2024-12-14 11:04:16 +0300

    Create Post Folder

---
+++ app/posts/page.js
@@ -0,0 +1,14 @@
+import { fetchPosts } from '../../utils/api';
+import PostList from '../../components/PostList';
+
+export default async function PostsPage() {
+  const posts = await fetchPosts();
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Posts</h1>
+      <PostList posts={posts} />
+    </div>
+  );
+}
+
